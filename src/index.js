551f37c49cdecbaa99b895f4fe7443d4df311eb8
Daniel Schmidt <danielmschmidt92@gmail.com>
2017-03-07 14:11:24 +0100

    fix(DrawerLayout): export of DrawerLayout
    This fixed errors with DrawerLayout.positions to be undefined

--- src/index.js
+++ src/index.js
@@ -9,7 +9,5 @@ import DrawerLayout from 'react-native-drawer-layout';
 if (Platform.OS === 'android') {
   module.exports = DrawerLayoutAndroid;
 } else {
-  module.exports = props => (
-    <DrawerLayout {...props} />
-  );
+  module.exports = DrawerLayout;
 }
